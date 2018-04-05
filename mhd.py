b8bef08a4ad5be9f9e05bfb06977f0799aa101e5
gk023 <35317501+gk023@users.noreply.github.com>
2018-04-04 22:56:37 -0400

    Updated version of code- still with some bugs

--- mhd.py
+++ mhd.py
@@ -14,12 +14,11 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-tf=10 #final time in seconds
-tstep=1 #size of the time step
-x0=2*np.pi #magnitude of min/max x value
-y0=2*np.pi #" y value
-z0=2*np.pi #" z value
-step=0.2*np.pi #step to create mesh
+tf=5 #final time in seconds
+tstep=0.01#size of the time step
+x0=10 #magnitude of min/max x value
+y0=10 #" y value
+step=1 #step to create mesh
 
 
 t = np.arange(0,tf+tstep,tstep) #set up time for problem
@@ -40,11 +39,11 @@ v0 = 1 #velocity
 P = -rho/4*(np.cos(2*X)+np.sin(2*Y))
 
 #set up the B field and velocity arrays
-bx = np.zeros((len(t),len(X),len(Y)))
-by = np.zeros((len(t),len(X),len(Y)))
+bx = np.zeros((len(t),len(x),len(y)))
+by = np.zeros((len(t),len(x),len(y)))
 
-vx = np.zeros((len(t),len(X),len(Y)))
-vy = np.zeros((len(t),len(X),len(Y)))
+vx = np.zeros((len(t),len(x),len(y)))
+vy = np.zeros((len(t),len(x),len(y)))
 
 
 
@@ -53,22 +52,54 @@ vy = np.zeros((len(t),len(X),len(Y)))
 bx[0] = b0*np.cos(2*X)*np.sin(2*Y)
 by[0] = -b0*np.sin(2*X)*np.cos(2*Y)
 
+
 vx[0] = v0*np.sin(X)*np.cos(Y)
 vy[0] = -v0*np.cos(X)*np.sin(Y)
 
 
+#Empty arrays to keep track of changes in time
+Ev=np.zeros(len(t))
+Eb=np.zeros(len(t))
 
+Ev[0]=0.5*(vx[0][0][0]**2+vy[0][0][0]**2)
+Eb[0]=0.5*(bx[0][0][0]**2+by[0][0][0]**2)
 
-#Euler Step for B and v to find how bz evolves in time
-#Working in the plane
+#Euler Step for B and v 
 
-for i in range(len(t)-1):
+i=0
+while i < len(t)-1:
 	for j in range(len(x)-1):
 		for k in range(len(y)-1):
-			bx[i+1][j][k] = bx[i][j][k]+vx[i][j][k]*(bx[i][j+1][k]-bx[i][j][k])/step-bx[i][j][k]*(vx[i][j+1][k]-vx[i][j][k])/step
-			by[i+1][j][k] = by[i][j][k]+vy[i][j][k]*(by[i][j][k+1]-by[i][j][k])/step-by[i][j][k]*(vy[i][j][k+1]-vy[i][j][k])/step
+			bx[i+1][j][k] = bx[i][j][k]+tstep*(-vx[i][j][k]*(bx[i][j+1][k]-bx[i][j][k])/step+bx[i][j][k]*(vx[i][j+1][k]-vx[i][j][k])/step)
+			by[i+1][j][k] = by[i][j][k]+tstep*(-vy[i][j][k]*(by[i][j][k+1]-by[i][j][k])/step+by[i][j][k]*(vy[i][j][k+1]-vy[i][j][k])/step)
+		
+			vx[i+1][j][k] = vx[i][j][k]+tstep*(vx[i][j][k]*(vx[i][j+1][k]-vx[i][j][k])/step-(P[j+1][k]-P[j][k])/step-by[i][j][k]*(by[i][j+1][k]-by[i][j][k])/step)
+			vy[i+1][j][k] = vy[i][j][k]+tstep*(vy[i][j][k]*(vy[i][j][k+1]-vy[i][j][k])/step-(P[j][k+1]-P[j][k])/step+bx[i][j][k]*(bx[i][j][k+1]-bx[i][j][k])/step)
 
-			vx[i+1][j][k] = vx[i][j][k]-vx[i][j][k]*(vx[i][j+1][k]-vx[i][j][k])/step-(P[j+1][k]-P[j][k])/step
-			vy[i+1][j][k] = vx[i][j][k]-vx[i][j][k]*(vy[i][j][k+1]-vy[i][j][k])/step-(P[j][k+1]-P[j][k])/step
-	 
-print(bx[5])
+
+
+	Ev[i+1]=0.5*(vx[i+1][0][0]**2+vy[i+1][0][0]**2)
+	Eb[i+1]=0.5*(bx[i+1][0][0]**2+by[i+1][0][0]**2)
+	i=i+1
+			
+print(bx[len(t)-1])
+
+fig0=plt.figure()
+ax1=fig0.add_subplot(1,1,1)
+ax2 = ax1.twinx()
+ax1.plot(t,Eb,'b-')
+ax2.plot(t,Ev,'r-')
+ax1.set_xlabel('Time (s)')
+ax1.set_ylabel('Kinetic Energy')
+ax2.set_ylabel('Magnetic Energy')
+
+
+
+plt.tight_layout()
+plt.show()
+plt.close()
+
+
+# plt.tight_layout()
+# plt.show()
+# plt.close()
