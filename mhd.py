d2edd6a0eecaf9f49a38a0ab6c6cadb0cec6cb43
gk023 <35317501+gk023@users.noreply.github.com>
2018-04-05 01:12:15 -0400

    More updates-infinite KE bug still an issue

--- mhd.py
+++ mhd.py
@@ -14,11 +14,11 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-tf=5 #final time in seconds
-tstep=0.01#size of the time step
-x0=10 #magnitude of min/max x value
-y0=10 #" y value
-step=1 #step to create mesh
+tf=0.05 #final time in seconds
+tstep=0.0001#size of the time step
+x0=2*np.pi #magnitude of min/max x value
+y0=2*np.pi #" y value
+step=0.2*np.pi #step to create mesh
 
 
 t = np.arange(0,tf+tstep,tstep) #set up time for problem
@@ -31,8 +31,8 @@ X,Y = np.meshgrid(x,y) #2D meshgrid
 
 
 #Define the parameter values
-rho = 1 #the density of the plasma
-b0 = 1 #the B field amplitude in Gaussian units
+rho = 1000 #the density of the plasma
+b0 = 1/np.sqrt(1000) #the B field amplitude in Gaussian units
 v0 = 1 #velocity
 
 #Specify the pressure field
@@ -61,37 +61,40 @@ vy[0] = -v0*np.cos(X)*np.sin(Y)
 Ev=np.zeros(len(t))
 Eb=np.zeros(len(t))
 
-Ev[0]=0.5*(vx[0][0][0]**2+vy[0][0][0]**2)
-Eb[0]=0.5*(bx[0][0][0]**2+by[0][0][0]**2)
+Ev[0]=0.5*(vx[0][10][10]**2+vy[0][10][10]**2)
+Eb[0]=0.5*(bx[0][10][10]**2+by[0][10][10]**2)
 
-#Euler Step for B and v 
+#Euler Step for B and v assuming fluid dominates
 
 i=0
 while i < len(t)-1:
-	for j in range(len(x)-1):
-		for k in range(len(y)-1):
-			bx[i+1][j][k] = bx[i][j][k]+tstep*(-vx[i][j][k]*(bx[i][j+1][k]-bx[i][j][k])/step+bx[i][j][k]*(vx[i][j+1][k]-vx[i][j][k])/step)
-			by[i+1][j][k] = by[i][j][k]+tstep*(-vy[i][j][k]*(by[i][j][k+1]-by[i][j][k])/step+by[i][j][k]*(vy[i][j][k+1]-vy[i][j][k])/step)
+	for j in range(1,len(x)-1):
+		for k in range(1,len(y)-1):
+			bx[i+1][j][k] = bx[i][j][k]+tstep*(by[i][j][k]*(vx[i][j][k+1]-vx[i][j][k-1])/(2*step)+vx[i][j][k]*(by[i][j][k+1]-by[i][j][k-1])/(2*step)-bx[i][j][k]*(vy[i][j][k+1]-vy[i][j][k-1])/(2*step)-vy[i][j][k]*(bx[i][j][k+1]-bx[i][j][k-1])/(2*step))
+			by[i+1][j][k] = by[i][j][k]+tstep*(-by[i][j][k]*(vx[i][j+1][k]-vx[i][j-1][k])/(2*step)-vx[i][j][k]*(by[i][j+1][k]-by[i][j-1][k])/(2*step)+bx[i][j][k]*(vy[i][j+1][k]-vy[i][j-1][k])/(2*step)+vy[i][j][k]*(bx[i][j+1][k]-bx[i][j-1][k])/(2*step))
 		
-			vx[i+1][j][k] = vx[i][j][k]+tstep*(vx[i][j][k]*(vx[i][j+1][k]-vx[i][j][k])/step-(P[j+1][k]-P[j][k])/step-by[i][j][k]*(by[i][j+1][k]-by[i][j][k])/step)
-			vy[i+1][j][k] = vy[i][j][k]+tstep*(vy[i][j][k]*(vy[i][j][k+1]-vy[i][j][k])/step-(P[j][k+1]-P[j][k])/step+bx[i][j][k]*(bx[i][j][k+1]-bx[i][j][k])/step)
+			vx[i+1][j][k] = vx[i][j][j]+tstep*(-vx[i][j][k]*(vx[i][j+1][k]-vx[i][j-1][k])/(2*step)-vy[i][j][k]*(vx[i][j][k+1]-vx[i][j][k-1])/(2*step)-(P[j+1][k]-P[j-1][k])/(2*step)-(P[j][k+1]-P[j][k-1])/(2*step)+vx[0][j][k])
+			vy[i+1][j][k] = vy[i][j][j]+tstep*(-vx[i][j][k]*(vy[i][j+1][k]-vy[i][j-1][k])/(2*step)-vy[i][j][k]*(vy[i][j][k+1]-vy[i][j][k-1])/(2*step)-(P[j+1][k]-P[j-1][k])/(2*step)-(P[j][k+1]-P[j][k-1])/(2*step)+vy[0][j][k])
 
 
 
-	Ev[i+1]=0.5*(vx[i+1][0][0]**2+vy[i+1][0][0]**2)
-	Eb[i+1]=0.5*(bx[i+1][0][0]**2+by[i+1][0][0]**2)
+	Ev[i+1]=0.5*(vx[i+1][10][10]**2+vy[i+1][10][10]**2)
+	Eb[i+1]=0.5*(bx[i+1][10][10]**2+by[i+1][10][10]**2)
 	i=i+1
 			
-print(bx[len(t)-1])
+
+print(by[100][10][10])
+
 
 fig0=plt.figure()
 ax1=fig0.add_subplot(1,1,1)
 ax2 = ax1.twinx()
-ax1.plot(t,Eb,'b-')
-ax2.plot(t,Ev,'r-')
+ax1.plot(t,Eb,'b-',label='Eb')
+ax2.plot(t,Ev,'r-',label='Ev')
 ax1.set_xlabel('Time (s)')
-ax1.set_ylabel('Kinetic Energy')
-ax2.set_ylabel('Magnetic Energy')
+ax1.set_ylabel('Magnetic Energy',color='b')
+ax2.set_ylabel('Kinetic Energy',color='r')
+
 
 
 
@@ -100,6 +103,3 @@ plt.show()
 plt.close()
 
 
-# plt.tight_layout()
-# plt.show()
-# plt.close()
