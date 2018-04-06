53517a85169af193315ac75320c22326cee322af
gk023 <35317501+gk023@users.noreply.github.com>
2018-04-05 23:16:26 -0400

    Actual update to code- last one was report

--- mhd.py
+++ mhd.py
@@ -12,31 +12,34 @@
 
 import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib.gridspec as gridspec
 
-
-tf=0.05 #final time in seconds
-tstep=0.0001#size of the time step
+tf=1.0#final time in seconds
+tstep=0.001#size of the time step
 x0=2*np.pi #magnitude of min/max x value
-y0=2*np.pi #" y value
-step=0.2*np.pi #step to create mesh
+y0=2*np.pi#" y value
+step=0.02*np.pi #step to create mesh
 
 
 t = np.arange(0,tf+tstep,tstep) #set up time for problem
 
-x = np.arange(-x0,x0+step,step) #Solving 2D mhd problem
-y = np.arange(-y0,y0+step,step)  
+x = np.arange(0,x0+step,step) #Solving 2D mhd problem
+y = np.arange(0,y0+step,step)  
 
 
 X,Y = np.meshgrid(x,y) #2D meshgrid
 
 
 #Define the parameter values
-rho = 1000 #the density of the plasma
-b0 = 1/np.sqrt(1000) #the B field amplitude in Gaussian units
-v0 = 1 #velocity
+rho = 1#the density of the plasma
+b0 = 0.35 #initial b field in Alfven units
+v0=0.35 #initial velocity
 
+P=np.zeros((len(x),len(y)))
 #Specify the pressure field
-P = -rho/4*(np.cos(2*X)+np.sin(2*Y))
+for i in range(len(x)):
+	for j in range(len(y)):
+		P[i][j] = -rho/4*(np.cos(2*x[i])+np.sin(2*y[j]))
 
 #set up the B field and velocity arrays
 bx = np.zeros((len(t),len(x),len(y)))
@@ -48,21 +51,24 @@ vy = np.zeros((len(t),len(x),len(y)))
 
 
 #Initial conditions of the plasma
+for i in range(len(x)):
+	for j in range(len(y)):
+		bx[0][i][j] = b0*np.sin(x[i])*np.cos(y[j])
+		by[0][i][j] = b0*np.cos(x[i])*np.sin(y[j])
 
-bx[0] = b0*np.cos(2*X)*np.sin(2*Y)
-by[0] = -b0*np.sin(2*X)*np.cos(2*Y)
 
-
-vx[0] = v0*np.sin(X)*np.cos(Y)
-vy[0] = -v0*np.cos(X)*np.sin(Y)
+		vx[0][i][j] = v0*np.sin(x[i])*np.cos(y[j])
+		vy[0][i][j] = -v0*np.cos(x[i])*np.sin(y[j])
 
 
 #Empty arrays to keep track of changes in time
 Ev=np.zeros(len(t))
 Eb=np.zeros(len(t))
 
-Ev[0]=0.5*(vx[0][10][10]**2+vy[0][10][10]**2)
-Eb[0]=0.5*(bx[0][10][10]**2+by[0][10][10]**2)
+cen = len(x)/2
+
+Ev[0]=0.5*(vx[0][cen][cen]**2+vy[0][cen][cen]**2)
+Eb[0]=0.5*(bx[0][cen][cen]**2+by[0][cen][cen]**2)
 
 #Euler Step for B and v assuming fluid dominates
 
@@ -70,20 +76,19 @@ i=0
 while i < len(t)-1:
 	for j in range(1,len(x)-1):
 		for k in range(1,len(y)-1):
-			bx[i+1][j][k] = bx[i][j][k]+tstep*(by[i][j][k]*(vx[i][j][k+1]-vx[i][j][k-1])/(2*step)+vx[i][j][k]*(by[i][j][k+1]-by[i][j][k-1])/(2*step)-bx[i][j][k]*(vy[i][j][k+1]-vy[i][j][k-1])/(2*step)-vy[i][j][k]*(bx[i][j][k+1]-bx[i][j][k-1])/(2*step))
-			by[i+1][j][k] = by[i][j][k]+tstep*(-by[i][j][k]*(vx[i][j+1][k]-vx[i][j-1][k])/(2*step)-vx[i][j][k]*(by[i][j+1][k]-by[i][j-1][k])/(2*step)+bx[i][j][k]*(vy[i][j+1][k]-vy[i][j-1][k])/(2*step)+vy[i][j][k]*(bx[i][j+1][k]-bx[i][j-1][k])/(2*step))
+			bx[i+1][j][k] = bx[i][j][k]+tstep*(-vx[i][j][k]*(bx[i][j+1][k]-bx[i][j-1][k])/(2*step)+bx[i][j][k]*(vx[i][j+1][k]-vx[i][j-1][k])/(2*step)+bx[0][j][k])
+			by[i+1][j][k] = by[i][j][k]+tstep*(-vy[i][j][k]*(by[i][j][k+1]-vy[i][j][k-1])/(2*step)+by[i][j][k]*(vy[i][j][k+1]-vy[i][j][k-1])/(2*step)+by[0][j][k])
 		
-			vx[i+1][j][k] = vx[i][j][j]+tstep*(-vx[i][j][k]*(vx[i][j+1][k]-vx[i][j-1][k])/(2*step)-vy[i][j][k]*(vx[i][j][k+1]-vx[i][j][k-1])/(2*step)-(P[j+1][k]-P[j-1][k])/(2*step)-(P[j][k+1]-P[j][k-1])/(2*step)+vx[0][j][k])
-			vy[i+1][j][k] = vy[i][j][j]+tstep*(-vx[i][j][k]*(vy[i][j+1][k]-vy[i][j-1][k])/(2*step)-vy[i][j][k]*(vy[i][j][k+1]-vy[i][j][k-1])/(2*step)-(P[j+1][k]-P[j-1][k])/(2*step)-(P[j][k+1]-P[j][k-1])/(2*step)+vy[0][j][k])
+			vx[i+1][j][k] = vx[i][j][k]+tstep*(-vx[i][j][k]*(vx[i][j+1][k]-vx[i][j-1][k])/(2*step)-(P[j+1][k]-P[j-1][k])/(2*step)-0.5*((bx[i][j+1][k]**2+by[i][j+1][k]**2-bx[i][j-1][k]**2-by[i][j-1][k]**2)/(2*step))+vx[0][j][k])
+			vy[i+1][j][k] = vy[i][j][k]+tstep*(-vy[i][j][k]*(vy[i][j][k+1]-vy[i][j][k-1])/(2*step)-(P[j][k+1]-P[j][k-1])/(2*step)-0.5*((bx[i][j][k+1]**2+by[i][j][k+1]**2-bx[i][j][k-1]**2-by[i][j][k-1]**2)/(2*step))+vx[0][j][k])
 
 
-
-	Ev[i+1]=0.5*(vx[i+1][10][10]**2+vy[i+1][10][10]**2)
-	Eb[i+1]=0.5*(bx[i+1][10][10]**2+by[i+1][10][10]**2)
+	Ev[i+1]=0.5*(vx[i+1][cen][cen]**2+vy[i+1][cen][cen]**2)
+	Eb[i+1]=0.5*(bx[i+1][cen][cen]**2+by[i+1][cen][cen]**2)
 	i=i+1
 			
 
-print(by[100][10][10])
+
 
 
 fig0=plt.figure()
@@ -103,3 +108,22 @@ plt.show()
 plt.close()
 
 
+
+
+fig1=plt.figure(figsize=(16,8))
+plt.xlabel('x')
+plt.ylabel('y')
+plt.streamplot(X, Y, vx[80],vy[80],color='k')
+
+
+
+plt.show()
+plt.close()
+
+fig2=plt.figure(figsize=(8,8))
+plt.xlabel('x')
+plt.ylabel('y')
+plt.imshow(np.sqrt(bx[80]**2+by[80]**2),cmap='plasma')
+
+plt.show()
+plt.close()
